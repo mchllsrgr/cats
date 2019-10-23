8eed8fb849a44505c501f784c887835b34518ff1
Michelle Siregar <michellesiregar@gmail.com>
2019-10-23 01:31:00 +0000

    initial commit

---
+++ asyncBreeds.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+
+const breedDetailsFromFile = function(breed, callback) {
+  fs.readFile(`./data/${breed}.txt`, 'utf8', (error, data) => {
+    if (!error) {
+      callback(data); 
+    } else {
+      return callback(undefined);
+    }
+  });
+}
+
+module.exports = breedDetailsFromFile;
